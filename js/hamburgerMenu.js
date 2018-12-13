2110d7750ef088d17c90456996e58df216047a77
Livia Bottoni <42958830+LiviaButtons@users.noreply.github.com>
2018-12-13 14:16:20 +0100

    sticky navbar, closes on click

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,19 +1,28 @@
 // Simple JavaScript for a hamburger menu on a responsive website 
 // Author: Livia Bottoni 
-
-
-//    // on click: 
-//    // if (button 'hamMenu' has attribute aria-hidden="false") {
-//        // show span with three bars
-//    // }
-//    // elseif (hamMenu is "open") {
-//        // replace the three spans of "icon-bar" with span of cross
-//        // since we have three individual icons of bars, maybe animate them into a cross?
-//    //}
+// based on: https://codepen.io/designcouch/pen/Atyop
 
 $(document).ready(function (event) {
+    // "open" and "close" hamburger Menu upon click
     $('#hamMenu').on("click", function (e) {
         $('#hamMenu').toggleClass('open');
         console.log (e.target);
     });
+    
+    $('.navbar-nav > *').on("click", function (e) {
+        // if any of the navigation links are clicked, close the menu
+        $('#hamMenu').toggleClass('open').toggleClass('collapsed');
+        $('#nav-content').attr('aria-expanded', 'false').toggleClass('in');
+        
+        // also, change the active attribute to the link that was clicked
+        $('.navbar-nav > *').removeClass('active');
+        console.log (e.target);
+        $current = e.target;
+        $parent = $($current).parent();
+        $($parent).addClass('active');
+    });
+    
+    // next step: change active class when scrolling
+    // AKA: once you pass #portfolio.height, switch the classes 
+    // or something along those lines
 });
